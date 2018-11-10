29bf6480beaa4543e0d34437e6f40d57ee6d4cc7
juanito Ramirez <juanitorivera2008@hotmail.com>
2018-11-10 00:56:52 -0600

    Db
    Conexion con base de datos

--- DB/connection.php
+++ DB/connection.php
@@ -1,11 +1,34 @@
-﻿<?php //Datos de conexión a la base de datos
-$hostname = 'localhost';
-$database = 'dbrocket';
-$username = 'root';
-$password = '';
-
-$mysqli = new mysqli($hostname, $username,$password, $database);
-if ($mysqli -> connect_errno) {
-die( "Fallo la conexión al servidor de MySQL: (" . $mysqli -> mysqli_connect_errno() 
-. ") " . $mysqli -> mysqli_connect_error());
-} ?>
+﻿<?php
+if(!empty($_POST['user_id'])){
+    $data = array();
+    
+    //database details
+    $dbHost     = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName     = 'dbrocket';
+    
+    //create connection and select DB
+    $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+    if($db->connect_error){
+        die("Unable to connect database: " . $db->connect_error);
+    }
+    
+    //get user data from the database
+    $query = $db->query("SELECT * FROM reportes");
+    
+    while($rows=$query->fetch_array()){
+        
+        $data['report'] = $rows['descripcion'];
+       // $data['lat'] = $rows['latitud'];
+        //$data['lon'] = $rows['latitud'];
+    }else{
+        $data['status'] = 'err';
+        $data['lan'] = '';
+        
+    }
+    
+    //returns data as JSON format
+    echo 'json_encode($data)';
+}
+?>
---
+++ DB/connection2.php
@@ -0,0 +1,15 @@
+﻿<?php
+$json=array();
+ $conexion=mysql_connect("localhost","root","","dbrocket");
+ $consulta= "SELECT * FROM reportes";
+ $resultado = mysql_query($conexion,$consulta);
+ if($consulta)
+ {
+    if($reg=mysql_fetch_array($resultado))
+    {
+        $json['datos'][]=$reg;
+    }
+    mysql_close($conexion);
+    echo json_encode($json);
+ }
+?>
