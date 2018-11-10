f0cfba3a34710e927ae0cebdd17d319b3739abd7
Cristian Lira <cristian.alberto287@gmail.com>
2018-11-10 11:56:44 -0600

    Update vista/Login_v1/js/crud.php

--- vista/Login_v1/js/crud.php
+++ vista/Login_v1/js/crud.php
@@ -4,14 +4,14 @@ include_once '../DB/connection.php';
 //Si se dio clic en guardar:
 if(isset($_POST['saveUser'])){
 	//Pasamos los parametros por medio de post
-	$newuser = $MySQLiconn->real_escape_string($_POST['user']);
-	$nombre =$MySQLiconn->real_escape_string($_POST['nombre']);   
-	$ape = $MySQLiconn->real_escape_string($_POST['ape']);
-	$pass = $MySQLiconn->real_escape_string($_POST['pass']);
-	$tel = $MySQLiconn->real_escape_string($_POST['telefono']);
+	$newuser = $db->real_escape_string($_POST['user']);
+	$nombre =$db->real_escape_string($_POST['nombre']);   
+	$ape = $db->real_escape_string($_POST['ape']);
+	$pass = $db->real_escape_string($_POST['pass']);
+	$tel = $db->real_escape_string($_POST['telefono']);
 
 	$contador=0;
-	$resultado = $MySQLiconn->query("SELECT usuario FROM users");
+	$resultado = $db->query("SELECT usuario FROM users");
 
 	while ($rows = $resultado->fetch_array()) {
 		if ($rows["usuario"]==$newuser) {
@@ -20,9 +20,9 @@ if(isset($_POST['saveUser'])){
 	}
 	if ($contador==0) {
 			//Realizamos la consulta
-	 		$SQL =$MySQLiconn->query("INSERT INTO users(usuario,pass, nombres, apellidos, telefono) VALUES('$newuser','$pass', '$nombre','$ape','$tel')");
+	 		$SQL =$db->query("INSERT INTO users(usuario,pass, nombres, apellidos, telefono) VALUES('$newuser','$pass', '$nombre','$ape','$tel')");
 	 		if(!$SQL){	 	//Mandar el mensaje de error
-				echo $MySQLiconn->error;
+				echo $db->error;
 	 		} 
 	 		else{
 	 			//Mandamos un mensaje de exito:
