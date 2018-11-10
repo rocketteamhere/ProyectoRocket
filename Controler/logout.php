363b4b9302b970271351803defc0615ca066f2a0
Cristian Lira <cristian.alberto287@gmail.com>
2018-11-09 19:01:42 -0600

    PHP

--- Controler/logout.php
+++ Controler/logout.php
@@ -2,7 +2,5 @@
 	session_start();
 		unset ($_SESSION['usuario']);
 			session_destroy();
-	 
-	/*include("../ingresar.php");*/
 	  echo "<META HTTP-EQUIV='REFRESH' CONTENT='0; URL=../index.php'>";
 	?>
